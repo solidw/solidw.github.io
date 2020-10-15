085f6c76b62028b32530dbb02bdac024bd252be3
solidw <solidw@naver.com>
2020-10-16 00:03:52 +0900

    chore: Change tabs to 2 space

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -4,15 +4,15 @@ import Link from 'next/link';
 import { User } from '#/interfaces';
 
 type Props = {
-	data: User;
+  data: User;
 };
 
 const ListItem: React.FC<Props> = ({ data }) => (
-	<Link href="/users/[id]" as={`/users/${data.id}`}>
-		<a>
-			{data.id}: {data.name}
-		</a>
-	</Link>
+  <Link href="/users/[id]" as={`/users/${data.id}`}>
+    <a>
+      {data.id}: {data.name}
+    </a>
+  </Link>
 );
 
 export default ListItem;
