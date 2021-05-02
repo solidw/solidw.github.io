bb12223cd6824718e3335800e85e495566e857bc
solidw <solidw@naver.com>
2021-05-03 00:19:53 +0900

    import reset.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import '#/styles/cssReset.css';
+import '#/styles/reset.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
