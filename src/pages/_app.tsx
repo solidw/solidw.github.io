3a96b885a2f1563e1b99721c960569a21c87a943
solidw <solidw@naver.com>
2021-05-09 23:58:51 +0900

    Include globals.scss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import '#/styles/reset.css';
+import '#/styles/_globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
