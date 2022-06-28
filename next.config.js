653158830549759de49ee00766b9108fd961926b
solidw <solidw@naver.com>
2022-06-29 00:31:22 +0900

    feat: Client side redirect 대신 server side

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   experimental: {
     esmExternals: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/posts",
+        permanent: true,
+      },
+    ];
+  },
 };
