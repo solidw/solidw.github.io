6c3114053d4c717c1dd8bdb8a3f266985ee44159
solidw <solidw@naver.com>
2022-07-16 20:33:48 +0900

    feat: 날짜가 아닌 시간으로 비교해서 정렬할 수 있도록 만들기 같은 날 여러 개 포스팅하면 순서대로 안올라감!

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,7 @@
 import { format } from "date-fns";
 
 export const dateUtils = {
-  formatDate: (date: Date) => format(date, "yyyy-MM-dd"),
+  formatDate: (date: Date) => date.toISOString(),
   formatDateToShow: (date: Date | string) =>
     format(typeof date === "string" ? new Date(date) : date, "yyyy. MM. dd."),
 };
